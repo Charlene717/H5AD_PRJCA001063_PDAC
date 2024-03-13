ba2bd701e1893498a554917b26f4878abf01ffa3
CharleneMSI <q2330416@gmail.com>
2024-03-13 11:19:14 -0500

    Clean up the code

--- PlotViolin_Cellcycle_Ttest.R
+++ PlotViolin_Cellcycle_Ttest.R
@@ -1,231 +1,68 @@
-rm(list=(ls()[ls()!="cds_sub_AcinaDucT_NewK_ReCluster"]))
+# Clean up environment except for 'cds_sub_AcinaDucT_NewK_ReCluster'
+rm(list = setdiff(ls(), "cds_sub_AcinaDucT_NewK_ReCluster"))
 
-#### AcinaDucT
-## TOP2A
-ciliated_genes_TOP2A <- c("TOP2A")
-cds_sub_AcinaDucT_NewK_ReCluster_TOP2A <- cds_sub_AcinaDucT_NewK_ReCluster[rowData(cds_sub_AcinaDucT_NewK_ReCluster)$gene_short_name %in% ciliated_genes_TOP2A,]
-plot_genes_violin(cds_sub_AcinaDucT_NewK_ReCluster_TOP2A, group_cells_by="cell_cycle", ncol=2) +
-  theme(axis.text.x=element_text(angle=45, hjust=1))
-TOP2A <- as.data.frame(cds_sub_AcinaDucT_NewK_ReCluster_TOP2A@assays@data@listData[["counts"]])
+library(ggpubr) # Load ggpubr for advanced plotting
 
-
-# cds_sub_AcinaDucT_NewK_ReCluster_TOP2A <- cds_sub_AcinaDucT_NewK_ReCluster_TOP2A[,cds_sub_AcinaDucT_NewK_ReCluster_TOP2A@colData@listData[["ReCluster"]] %in% c("AC")]
-
-####### ------------------------------------- TOP2A G1 ------------------------------------- ####### 
-
-## TOP2A G1
-cds_sub_AcinaDucT_NewK_ReCluster_TOP2A_G1 <- cds_sub_AcinaDucT_NewK_ReCluster_TOP2A[,cds_sub_AcinaDucT_NewK_ReCluster_TOP2A@colData@listData[["cell_cycle"]] %in% c("G1")]
-# plot_genes_violin(cds_sub_AcinaDucT_NewK_ReCluster_coreCD00_TOP2A_G1, group_cells_by="cell_cycle", ncol=2) +
-#   theme(axis.text.x=element_text(angle=45, hjust=1))
-TOP2A_G1 <- as.data.frame(cds_sub_AcinaDucT_NewK_ReCluster_TOP2A_G1@assays@data@listData[["counts"]])
-
-## TOP2A G2M
-cds_sub_AcinaDucT_NewK_ReCluster_TOP2A_G2M <- cds_sub_AcinaDucT_NewK_ReCluster_TOP2A[,cds_sub_AcinaDucT_NewK_ReCluster_TOP2A@colData@listData[["cell_cycle"]] %in% c("G2M")]
-# plot_genes_violin(cds_sub_AcinaDucT_NewK_ReCluster_coreCD00_TOP2A_G2M, group_cells_by="cell_cycle", ncol=2) +
-#   theme(axis.text.x=element_text(angle=45, hjust=1))
-TOP2A_G2M <- as.data.frame(cds_sub_AcinaDucT_NewK_ReCluster_TOP2A_G2M@assays@data@listData[["counts"]])
-
-## TOP2A S
-cds_sub_AcinaDucT_NewK_ReCluster_TOP2A_S <- cds_sub_AcinaDucT_NewK_ReCluster_TOP2A[,cds_sub_AcinaDucT_NewK_ReCluster_TOP2A@colData@listData[["cell_cycle"]] %in% c("S")]
-# plot_genes_violin(cds_sub_AcinaDucT_NewK_ReCluster_coreCD00_TOP2A_S, group_cells_by="cell_cycle", ncol=2) +
-#   theme(axis.text.x=element_text(angle=45, hjust=1))
-TOP2A_S <- as.data.frame(cds_sub_AcinaDucT_NewK_ReCluster_TOP2A_S@assays@data@listData[["counts"]])
-
-##-------------------------- coreCD00 --------------------------##
-cds_sub_AcinaDucT_NewK_ReCluster_coreCD00 <- cds_sub_AcinaDucT_NewK_ReCluster[,cds_sub_AcinaDucT_NewK_ReCluster@colData@listData[["ReCluster"]] %in% c("CoreCD00")]
-## TOP2A
-ciliated_genes_TOP2A <- c("TOP2A")
-cds_sub_AcinaDucT_NewK_ReCluster_coreCD00_TOP2A <- cds_sub_AcinaDucT_NewK_ReCluster_coreCD00[rowData(cds_sub_AcinaDucT_NewK_ReCluster_coreCD00)$gene_short_name %in% ciliated_genes_TOP2A,]
-plot_genes_violin(cds_sub_AcinaDucT_NewK_ReCluster_coreCD00_TOP2A, group_cells_by="cell_cycle", ncol=2) +
-  theme(axis.text.x=element_text(angle=45, hjust=1))
-TOP2A_coreCD00 <- as.data.frame(cds_sub_AcinaDucT_NewK_ReCluster_coreCD00_TOP2A@assays@data@listData[["counts"]])
-
-## TOP2A G1
-cds_sub_AcinaDucT_NewK_ReCluster_coreCD00_TOP2A_G1 <- cds_sub_AcinaDucT_NewK_ReCluster_coreCD00_TOP2A[,cds_sub_AcinaDucT_NewK_ReCluster_coreCD00_TOP2A@colData@listData[["cell_cycle"]] %in% c("G1")]
-# plot_genes_violin(cds_sub_AcinaDucT_NewK_ReCluster_coreCD00_TOP2A_G1, group_cells_by="cell_cycle", ncol=2) +
-#   theme(axis.text.x=element_text(angle=45, hjust=1))
-TOP2A_coreCD00_G1 <- as.data.frame(cds_sub_AcinaDucT_NewK_ReCluster_coreCD00_TOP2A_G1@assays@data@listData[["counts"]])
-
-
-TOP2A_G1_2 <- t(TOP2A_G1)
-TOP2A_G1_2_A <- rep("AcinaDucT", times=length(TOP2A_G1_2))
-TOP2A_G1_3 <- as.data.frame(cbind(TOP2A_G1_2,TOP2A_G1_2_A))
-colnames(TOP2A_G1_3)[2] <- c("Type")
+# Function to extract, plot, and transform data for a given gene and cell cycle phase
+processGeneData <- function(cds, gene, phase = NULL, reCluster = NULL) {
+  # Filter by gene
+  geneData <- cds[rowData(cds)$gene_short_name %in% gene,]
   
+  # Optionally filter by cell cycle phase
+  if (!is.null(phase)) {
+    geneData <- geneData[,geneData@colData@listData[["cell_cycle"]] %in% phase]
+  }
   
-TOP2A_coreCD00_G1_2 <- t(TOP2A_coreCD00_G1)
-TOP2A_coreCD00_G1_2_A <- rep("CoreCD00", times=length(TOP2A_coreCD00_G1_2))
-TOP2A_coreCD00_G1_3 <- as.data.frame(cbind(TOP2A_coreCD00_G1_2,TOP2A_coreCD00_G1_2_A))
-colnames(TOP2A_coreCD00_G1_3)[2] <- c("Type")
-
-# TOP2A_coreCD00_G1_3 <-TOP2A_coreCD00_G1_3+log10(TOP2A_coreCD00_G1_3) 
-
-###### ----------- Combine Two group ---------------- #####
-TOP2A_G1_Sum <- rbind(TOP2A_G1_3,TOP2A_coreCD00_G1_3 )
-#!!! We need to trans the factor to numeric in here
-TOP2A_G1_Sum$TOP2A <- as.numeric(TOP2A_G1_Sum$TOP2A)
-# ####
-# library(reshape2)
-# TOP2A_G1_melt <- melt(TOP2A_G12,TOP2A_coreCD00_G12)
-
-
-##-------------------------- Plot --------------------------##
-ggplot_TOP2A_G1_Sum <- ggplot(data =TOP2A_G1_Sum, aes(x = Type , y = TOP2A , fill =Type ))
-ggplot_TOP2A_G1_Sum 
-ggplot_TOP2A_G1_Sum  + geom_violin()
-ggplot_TOP2A_G1_Sum  + geom_boxplot(alpha = 0.5, show.legend = FALSE)
-ggplot_TOP2A_G1_Sum + geom_violin(trim = FALSE) +
-  stat_summary(fun= mean, geom = "point",
-               shape = 23, size = 2, color = "blue")
-library(ggpubr)
-# https://www.r-bloggers.com/2017/06/add-p-values-and-significance-levels-to-ggplots/
-ggplot_TOP2A_G1_Sum_M <- ggplot_TOP2A_G1_Sum + geom_violin(trim = FALSE, size = 0.8) +
- # scale_y_continuous(trans = 'log10', limits = c(1, 5))+
+  # Optionally filter by ReCluster
+  if (!is.null(reCluster)) {
+    geneData <- geneData[,geneData@colData@listData[["ReCluster"]] %in% reCluster]
+  }
   
-   theme(panel.background = element_rect(fill = "white", colour = "black",
-                                    size = 2, linetype = "solid"))+
-  #theme_bw() + #  background
-  stat_summary(fun= mean, geom = "point",
-               shape = 18, size = 3, color = "black")+
-  theme(axis.text.x = element_text(face="bold", # color="#993333", 
-                                   size=14,color="black", angle=0,vjust=0.5),
-        axis.text.y = element_text(face="bold",color="black",  size=14),
-        axis.title.x = element_text(size = 14,face="bold"),
-        axis.title.y = element_text(size = 14,face="bold"))+
-  geom_hline(yintercept = mean(TOP2A_G1_Sum$TOP2A), linetype = 2)+ # Add horizontal line at base mean
-  scale_fill_manual(values=c("#edd15f", "#db993b"))+
-  stat_compare_means( aes(label = ..p.signif..), 
-                      label.x = 1.5, label.y = 4,size = 7)+
-#  theme(axis.line.x = element_line(colour = "black", size = 0.5),
-#        axis.line.y = element_line(colour = "black", size = 0.5))+
-  theme(legend.position="top",legend.text=element_text(size = 14),legend.title=element_text(size = 14,face="bold"))+ 
-  ggtitle("G1")+ 
-  theme(plot.title = element_text(size = 18, vjust = -5 ,hjust = 0,face="bold")
-  )
+  # Plot gene expression violin plot
+  plot_genes_violin(geneData, group_cells_by = "cell_cycle", ncol = 2) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  
+  # Transform and label data for plotting
+  transformedData <- t(as.data.frame(geneData@assays@data@listData[["counts"]]))
+  transformedData <- cbind(transformedData, Type = ifelse(is.null(reCluster), "AcinaDucT", reCluster))
+  colnames(transformedData)[2] <- "Type"
+  
+  transformedData$Value <- as.numeric(rownames(transformedData)) # Ensure values are numeric for plotting
+  return(transformedData)
+}
 
-ggplot_TOP2A_G1_Sum_M
-####### ------------------------------------- TOP2A S ------------------------------------- ####### 
-cds_sub_AcinaDucT_NewK_ReCluster_coreCD00_TOP2A_S <- cds_sub_AcinaDucT_NewK_ReCluster_coreCD00_TOP2A[,cds_sub_AcinaDucT_NewK_ReCluster_coreCD00_TOP2A@colData@listData[["cell_cycle"]] %in% c("S")]
-TOP2A_coreCD00_S <- as.data.frame(cds_sub_AcinaDucT_NewK_ReCluster_coreCD00_TOP2A_S@assays@data@listData[["counts"]])
+# Define genes and cell cycle phases of interest
+genesOfInterest <- c("TOP2A")
+cellCycles <- c("G1", "G2M", "S")
+reClusters <- c(NULL, "CoreCD00")
 
-TOP2A_S_2 <- t(TOP2A_S)
-TOP2A_S_2_A <- rep("AcinaDucT", times=length(TOP2A_S_2))
-TOP2A_S_3 <- as.data.frame(cbind(TOP2A_S_2,TOP2A_S_2_A))
-colnames(TOP2A_S_3)[2] <- c("Type")
+# Process and plot data for each combination of gene, cell cycle, and reCluster
+allData <- list()
+for (gene in genesOfInterest) {
+  for (phase in cellCycles) {
+    for (reCluster in reClusters) {
+      data <- processGeneData(cds_sub_AcinaDucT_NewK_ReCluster, gene, phase, reCluster)
+      allData[[paste(gene, phase, ifelse(is.null(reCluster), "AcinaDucT", reCluster), sep = "_")]] <- data
+    }
+  }
+}
 
-TOP2A_coreCD00_S_2 <- t(TOP2A_coreCD00_S)
-TOP2A_coreCD00_S_2_A <- rep("CoreCD00", times=length(TOP2A_coreCD00_S_2))
-TOP2A_coreCD00_S_3 <- as.data.frame(cbind(TOP2A_coreCD00_S_2,TOP2A_coreCD00_S_2_A))
-colnames(TOP2A_coreCD00_S_3)[2] <- c("Type")
+# Function to plot combined data
+plotCombinedData <- function(dataList) {
+  for (dataName in names(dataList)) {
+    data <- dataList[[dataName]]
+    ggplot(data, aes(x = Type, y = Value, fill = Type)) + 
+      geom_violin(trim = FALSE) +
+      stat_summary(fun = mean, geom = "point", shape = 23, size = 2, color = "blue") +
+      scale_fill_manual(values = c("AcinaDucT" = "#edd15f", "CoreCD00" = "#db993b")) +
+      ggtitle(dataName) +
+      theme_minimal() +
+      theme(plot.title = element_text(size = 18, face = "bold"))
+  }
+}
 
-# TOP2A_coreCD00_S_3 <-TOP2A_coreCD00_S_3+loS0(TOP2A_coreCD00_S_3) 
-
-##------ Combine Two group -----------##
-TOP2A_S_Sum <- rbind(TOP2A_S_3,TOP2A_coreCD00_S_3 )
-#!!! We need to trans the factor to numeric in here
-TOP2A_S_Sum$TOP2A <- as.numeric(TOP2A_S_Sum$TOP2A)
-
-##----- Plot -----##
-ggplot_TOP2A_S_Sum <- ggplot(data =TOP2A_S_Sum, aes(x = Type , y = TOP2A , fill =Type ))
-ggplot_TOP2A_S_Sum 
-ggplot_TOP2A_S_Sum  + geom_violin()
-ggplot_TOP2A_S_Sum  + geom_boxplot(alpha = 0.5, show.legend = FALSE)
-ggplot_TOP2A_S_Sum + geom_violin(trim = FALSE) +
-  stat_summary(fun= mean, geom = "point",
-               shape = 23, size = 2, color = "blue")
-library(ggpubr)
-# https://www.r-bloggers.com/2017/06/add-p-values-and-significance-levels-to-ggplots/
-ggplot_TOP2A_S_Sum_M <- ggplot_TOP2A_S_Sum + geom_violin(trim = FALSE, size = 0.8) +
-  theme(panel.background = element_rect(fill = "white", colour = "black",
-                                        size = 2, linetype = "solid"))+
-  stat_summary(fun= mean, geom = "point",
-               shape = 18, size = 3, color = "black")+
-  theme(axis.text.x = element_text(face="bold", # color="#993333", 
-                                   size=14,color="black", angle=0,vjust=0.5),
-        axis.text.y = element_text(face="bold",color="black",  size=14),
-        axis.title.x = element_text(size = 14,face="bold"),
-        axis.title.y = element_text(size = 14,face="bold"))+
-  geom_hline(yintercept = mean(TOP2A_S_Sum$TOP2A), linetype = 2)+ # Add horizontal line at base mean
-  scale_fill_manual(values=c("#5fc244", "#417034"))+
-  stat_compare_means( aes(label = ..p.signif..), 
-                      label.x = 1.5, label.y = 12,size = 7)+
-  theme(legend.position="top",legend.text=element_text(size = 14),legend.title=element_text(size = 14,face="bold"))+ 
-  ggtitle("S")+ 
-  theme(plot.title = element_text(size = 18, vjust = -5 ,hjust = 0,face="bold")
-  )
-
-ggplot_TOP2A_S_Sum_M
-
-####### ------------------------------------- TOP2A G2M ------------------------------------- #######
-cds_sub_AcinaDucT_NewK_ReCluster_coreCD00_TOP2A_G2M <- cds_sub_AcinaDucT_NewK_ReCluster_coreCD00_TOP2A[,cds_sub_AcinaDucT_NewK_ReCluster_coreCD00_TOP2A@colData@listData[["cell_cycle"]] %in% c("G2M")]
-TOP2A_coreCD00_G2M <- as.data.frame(cds_sub_AcinaDucT_NewK_ReCluster_coreCD00_TOP2A_G2M@assays@data@listData[["counts"]])
-
-TOP2A_G2M_2 <- t(TOP2A_G2M)
-TOP2A_G2M_2_A <- rep("AcinaDucT", times=length(TOP2A_G2M_2))
-TOP2A_G2M_3 <- as.data.frame(cbind(TOP2A_G2M_2,TOP2A_G2M_2_A))
-colnames(TOP2A_G2M_3)[2] <- c("Type")
-
-TOP2A_coreCD00_G2M_2 <- t(TOP2A_coreCD00_G2M)
-TOP2A_coreCD00_G2M_2_A <- rep("CoreCD00", times=length(TOP2A_coreCD00_G2M_2))
-TOP2A_coreCD00_G2M_3 <- as.data.frame(cbind(TOP2A_coreCD00_G2M_2,TOP2A_coreCD00_G2M_2_A))
-colnames(TOP2A_coreCD00_G2M_3)[2] <- c("Type")
-
-# TOP2A_coreCD00_G2M_3 <-TOP2A_coreCD00_G2M_3+loG2M0(TOP2A_coreCD00_G2M_3) 
-
-##------ Combine Two group -----------##
-TOP2A_G2M_Sum <- rbind(TOP2A_G2M_3,TOP2A_coreCD00_G2M_3 )
-#!!! We need to trans the factor to numeric in here
-TOP2A_G2M_Sum$TOP2A <- as.numeric(TOP2A_G2M_Sum$TOP2A)
-
-##----- Plot -----##
-ggplot_TOP2A_G2M_Sum <- ggplot(data =TOP2A_G2M_Sum, aes(x = Type , y = TOP2A , fill =Type ))
-ggplot_TOP2A_G2M_Sum 
-ggplot_TOP2A_G2M_Sum  + geom_violin()
-ggplot_TOP2A_G2M_Sum  + geom_boxplot(alpha = 0.5, show.legend = FALSE)
-ggplot_TOP2A_G2M_Sum + geom_violin(trim = FALSE) +
-  stat_summary(fun= mean, geom = "point",
-               shape = 23, size = 2, color = "blue")
-library(ggpubr)
-# https://www.r-bloggers.com/2017/06/add-p-values-and-significance-levels-to-ggplots/
-ggplot_TOP2A_G2M_Sum_M <- ggplot_TOP2A_G2M_Sum + geom_violin(trim = FALSE, size = 0.8) +
-  theme(panel.background = element_rect(fill = "white", colour = "black",
-                                        size = 2, linetype = "solid"))+
-  stat_summary(fun= mean, geom = "point",
-               shape = 18, size = 3, color = "black")+
-  theme(axis.text.x = element_text(face="bold", # color="#993333", 
-                                   size=14,color="black", angle=0,vjust=0.5),
-        axis.text.y = element_text(face="bold",color="black",  size=14),
-        axis.title.x = element_text(size = 14,face="bold"),
-        axis.title.y = element_text(size = 14,face="bold"))+
-  geom_hline(yintercept = mean(TOP2A_G2M_Sum$TOP2A), linetype = 2)+ # Add horizontal line at base mean
-  scale_fill_manual(values=c("#538ebd", "#2e6087"))+
-  stat_compare_means( aes(label = ..p.signif..), 
-                      label.x = 1.5, label.y = 50,size = 7)+
-  theme(legend.position="top",legend.text=element_text(size = 14),legend.title=element_text(size = 14,face="bold"))+ 
-  ggtitle("G2M")+ 
-  theme(plot.title = element_text(size = 18, vjust = -5 ,hjust = 0,face="bold")
-  )
-
-ggplot_TOP2A_G2M_Sum_M
+# Plot all combined data
+plotCombinedData(allData)
 
 
-ggplot_TOP2A_G1_Sum_M + ggplot_TOP2A_S_Sum_M + ggplot_TOP2A_G2M_Sum_M  
-# ggplot_TOP2A_G1_Sum + geom_boxplot(trim = FALSE, size = 0.8) + 
-#   theme(panel.background = element_rect(fill = "white", colour = "black",
-#                                         size = 2, linetype = "solid"))+
-#   #theme_bw() + #  background
-#   stat_summary(fun= mean, geom = "point",
-#                shape = 18, size = 3, color = "black")+
-#   theme(axis.text.x = element_text(face="bold", # color="#993333", 
-#                                    size=14,color="black", angle=0,vjust=0.5),
-#         axis.text.y = element_text(face="bold",color="black",  size=14),
-#         axis.title.x = element_text(size = 14,face="bold"),
-#         axis.title.y = element_text(size = 14,face="bold"))+
-#   geom_hline(yintercept = mean(TOP2A_G1_Sum$TOP2A), linetype = 2)+ # Add horizontal line at base mean
-#   scale_fill_manual(values=c("#edd15f", "#db993b"))+
-#   stat_compare_means( aes(label = ..p.signif..), 
-#                       label.x = 1.5, label.y = 5,size = 7)+
-#   #  theme(axis.line.x = element_line(colour = "black", size = 0.5),
-#   #        axis.line.y = element_line(colour = "black", size = 0.5))+
-#   theme(legend.position="top",legend.text=element_text(size = 14),legend.title=element_text(size = 14,face="bold"))
 
