8cff5d3b9eee6156f0b4e81c46162df5e2086868
CharleneMSI <q2330416@gmail.com>
2024-03-13 11:38:04 -0500

    Clean up the code

--- Monocle3_To_Seurat.R
+++ Monocle3_To_Seurat.R
@@ -1,4 +1,4 @@
-## Load package
+# Load necessary libraries
 library(Seurat)
 library(SummarizedExperiment) 
 library(AnnotationDbi)
@@ -7,53 +7,43 @@ library('org.Hs.eg.db')
 library(Hmisc)
 library(monocle3)
 
-###### Convert Monocle3 Object to Seurat Object ######
-
-Monocle3_To_Seurat <- function(cds,FigureName) {
-CCdata <- cds@assays@data@listData$counts
-colnames(CCdata) = cds@assays@data@listData[["counts"]]@Dimnames[[2]]
-rownames(CCdata) = cds@assays@data@listData[["counts"]]@Dimnames[[1]]
-
-DataCellcycle <- CCdata
-DataCellcycle <- as(as.matrix(DataCellcycle), "dgCMatrix")
-
-marrow <- CreateSeuratObject(counts = DataCellcycle)
-marrow <- NormalizeData(marrow)
-marrow <- FindVariableFeatures(marrow, selection.method = "vst")
-marrow <- ScaleData(marrow, features = rownames(marrow))
-
-marrow@assays[["RNA"]]@counts@Dimnames[[1]] <- cds@assays@data@listData[["counts"]]@Dimnames[[1]]
-marrow@assays[["RNA"]]@data@Dimnames[[1]] <- cds@assays@data@listData[["counts"]]@Dimnames[[1]]
-marrow@commands[["ScaleData.RNA"]]@params[["features"]] <- cds@assays@data@listData[["counts"]]@Dimnames[[1]]
-
-## Run PCA
-marrow <- RunPCA(marrow, features = VariableFeatures(marrow), ndims.print = 1:10, nfeatures.print = 10)
-DimHeatmap(marrow, dims = c(1, 2))
-
-png(paste0(PathName,"/",RVersion,"/",RVersion,"_",FigureName,"_CC_DimHP1to10.png")) # 設定輸出圖檔
-DimHeatmap(marrow, dims = c(1: 10))
-dev.off() # 關閉輸出圖檔
-
-png(paste0(PathName,"/",RVersion,"/",RVersion,"_",FigureName,"_CC_DimHP1&2.png")) # 設定輸出圖檔
-DimHeatmap(marrow, dims = c(1, 2))
-dev.off() # 關閉輸出圖檔
-
-png(paste0(PathName,"/",RVersion,"/",RVersion,"_",FigureName,"_CC_DimHP3&4.png")) # 設定輸出圖檔
-DimHeatmap(marrow, dims = c(3, 4))
-dev.off() # 關閉輸出圖檔
-
-png(paste0(PathName,"/",RVersion,"/",RVersion,"_",FigureName,"_CC_DimHP5&6.png")) # 設定輸出圖檔
-DimHeatmap(marrow, dims = c(5, 6))
-dev.off() # 關閉輸出圖檔
-
-png(paste0(PathName,"/",RVersion,"/",RVersion,"_",FigureName,"_CC_DimHP7&8.png")) # 設定輸出圖檔
-DimHeatmap(marrow, dims = c(7, 8))
-dev.off() # 關閉輸出圖檔
-
-png(paste0(PathName,"/",RVersion,"/",RVersion,"_",FigureName,"_CC_DimHP9&10.png")) # 設定輸出圖檔
-DimHeatmap(marrow, dims = c(9, 10))
-dev.off() # 關閉輸出圖檔
-
-return(marrow)
+# Convert Monocle3 Object to Seurat Object function
+Monocle3_To_Seurat <- function(cds, FigureName, outputPath) {
+  # Extract and prepare count data
+  CCdata <- cds@assays@data@listData$counts
+  colnames(CCdata) <- colnames(CCdata)
+  rownames(CCdata) <- rownames(CCdata)
+  DataCellcycle <- as(as.matrix(CCdata), "dgCMatrix")
+  
+  # Create Seurat object and preprocess data
+  marrow <- CreateSeuratObject(counts = DataCellcycle)
+  marrow <- NormalizeData(marrow)
+  marrow <- FindVariableFeatures(marrow, selection.method = "vst")
+  marrow <- ScaleData(marrow, features = rownames(marrow))
+  
+  # Ensure consistency of row names
+  rowDataNames <- rownames(CCdata)
+  marrow@assays[["RNA"]]@counts@Dimnames[[1]] <- rowDataNames
+  marrow@assays[["RNA"]]@data@Dimnames[[1]] <- rowDataNames
+  marrow@commands[["ScaleData.RNA"]]@params[["features"]] <- rowDataNames
+  
+  # Run PCA and plot Dimensional Heatmaps
+  marrow <- RunPCA(marrow, features = VariableFeatures(marrow), ndims.print = 1:10, nfeatures.print = 10)
+  
+  # Plotting Dimensional Heatmaps for each specified dimension pair and save
+  for (dims in list(c(1, 2), c(3, 4), c(5, 6), c(7, 8), c(9, 10))) {
+    dimPairName <- paste(dims, collapse = "&")
+    plotPath <- paste0(outputPath, "/", RVersion, "_", FigureName, "_CC_DimHP", dimPairName, ".png")
+    png(plotPath)
+    DimHeatmap(marrow, dims = dims)
+    dev.off()
+  }
+  
+  return(marrow)
 }
+
+# Example usage:
+# marrow <- Monocle3_To_Seurat(cds = myCDS, FigureName = "MyFigure", outputPath = "path/to/output")
+
+
 
